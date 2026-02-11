1b760f9a8990a4f529e5bd408456161fd0d8ab27
SaqinNoor <akmsaqinnoor@gmail.com>
2026-02-12 03:02:31 +0600

    Fix subpage build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/IELC-Homepage/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ec: resolve(__dirname, 'ec.html'),
+        events: resolve(__dirname, 'events.html'),
+      },
+    },
+  },
 })
