f8c617e72a3c59a28c5b060a102eaa54c62863bc
SaqinNoor <akmsaqinnoor@gmail.com>
2026-02-15 19:14:30 +0600

    Fix 404

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         ec: resolve(__dirname, 'ec.html'),
+        '8th-ec': resolve(__dirname, '8th-ec.html'),
+        '7th-ec': resolve(__dirname, '7th-ec.html'),
+        '6th-ec': resolve(__dirname, '6th-ec.html'),
+        '5th-ec': resolve(__dirname, '5th-ec.html'),
         events: resolve(__dirname, 'events.html'),
       },
     },
